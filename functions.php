83b45418342f98931c49c2416416327d0215afe9
griminir <viktor.elliot.king@gmail.com>
2024-11-29 11:00:10 +0100

    the use of static variables

--- functions.php
+++ functions.php
@@ -37,3 +37,40 @@ function fooMath2(int $x, int $y, int $z): int {
 }
 
 echo fooMath2(y: 10, x: 5, z: 15);
+
+// scope
+$fooGlobal = 'foo';
+
+function scope3(): string {
+    // this now a reference to the global variable
+    // any changes to $fooGlobal inside the function will be reflected in the global variable
+    // but only if this function is called first
+    global $fooGlobal;
+    $fooGlobal = 'fooLocalChange';
+    return $fooGlobal;
+}
+// echo scope3();
+echo $fooGlobal;
+echo $GLOBALS['fooGlobal']; // $GLOBALS is a superglobal using the variable name as the key
+// try and not use superglobals and global makes code harder to read
+// use parameters and return values instead
+
+// static variables kinda like a cache
+function getValue(): int {
+    static $value = null;
+    if($value === null) {
+        $value = expensiveFuntion();
+    }
+    // do something to the value
+    return $value;
+}
+
+function expensiveFuntion(): int {
+    sleep(2);
+    echo 'This is an expensive function'; // this will only be called once because of the static variable
+    return 10;
+};
+
+echo getValue() . '<br>';
+echo getValue() . '<br>';
+echo getValue() . '<br>';
