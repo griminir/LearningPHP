2a3e61b6d789b37eadb98210bcde9d3a805aaa5d
griminir <viktor.elliot.king@gmail.com>
2024-12-02 14:14:19 +0100

    it cant be seen here but i spend hours learning about virtual hosts, here is also some basic error handeling

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 // inlcude does not stop the execution of the script if the file is not found
 // require stops the execution of the script if the file is not found
 // require_once and include_once are used to include the file only once 
@@ -9,3 +9,22 @@
     #require 'switch.php';
     #require 'switch.php';
     #echo 'Hello World!';
+
+// php.ini file is the configuration file for PHP
+// https://www.php.net/manual/en/ini.list.php
+// ini_get();
+// ini_set();
+// if it says ini_perdir or ini_system it means that it can not be changed in the script
+
+// error handeling
+// trigger_error('example error', E_USER_WARNING);
+// function errorHandler(int $type, string $msg, ?string $file, ?int $line)
+// {
+//     echo "$type: $msg in $file on line $line";
+//     exit;
+// }
+
+// set_error_handler('errorHandler', E_ALL);
+// echo $z;
+
+echo 'hello world it works from localhost';
